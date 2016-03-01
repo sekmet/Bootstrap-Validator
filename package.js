01c4603eafb83932d67edc29ee062c09212232a8
Carlos Guimaraes <sekmet@open4g.com>
2016-03-01 17:20:10 -0300

    package.js Updates

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  name: 'rgoomar:bootstrap-validator',
+  name: 'sekmet:bootstrap-validator',
   summary: 'bootstrapValidator to validate forms and use bootstrap 3 to show errors',
   version: '0.5.3',
   git: 'https://github.com/sekmet/Bootstrap-Validator'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("METEOR@0.9.0");
+    api.versionsFrom('1.2.1');
     api.use('jquery', 'client');
     api.addFiles('lib/js/bootstrapValidator.js', 'client');
+    api.addFiles('lib/js/language/en_US.js', 'client');
     api.addFiles('lib/css/bootstrapValidator.css', 'client');
 });
